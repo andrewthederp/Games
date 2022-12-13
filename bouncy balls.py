af1a70179b39adf33187875c376e3f2c74c76efd
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-12-13 03:18:12 +0200

    Update bouncy balls.py

--- bouncy balls.py
+++ bouncy balls.py
@@ -34,7 +34,7 @@ bigger_mouse_rect = pygame.Rect((0,0),(100,100))
 flying_circles = []
 score_rect = False
 score = 0
-# Button
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
